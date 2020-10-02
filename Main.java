9a06410aef38c5ec834509fa2547b9cdfac6d912
Jett Kim <jettkim1@gmail.com>
2020-10-02 08:14:49 -0700

    currency

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@
 import java.util.*;
 import java.lang.Math;
 import java.util.Random;
-
+import java.text.DecimalFormat;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
@@ -44,6 +44,10 @@ class Main {
       }
 
       switch (input) {
+
+      case "currency":
+        currency_command();
+        break;
 
       case "add":
         add_command();
@@ -193,6 +197,105 @@ class Main {
     System.out.println("logged in as '" + account.username + "'");
     System.out.println("Use 'help' for a list of commands!");
   }
+  
+  public static void currency_command() {
+
+    double amount, dollar, pound, code, euro, yen, rupee;
+ 
+		DecimalFormat f = new DecimalFormat("##.##");
+ 
+		Scanner sc = new Scanner(System.in);
+ 
+		System.out.println("hi, Welcome to the Currency Converter!");
+ 
+		System.out.println("which currency You want to Convert ? ");
+		
+		System.out.println("1:Ruppe \t2:Dollar \t3:Pound \n4:Euro \t5:Yen");
+		code = sc.nextInt();
+		
+		System.out.println("How much Money you want to convert?");
+		amount = sc.nextFloat();
+ 
+		// For amounts Conversion
+		if (code == 1) {
+ 
+			dollar = amount / 70;
+			System.out.println("Your " + amount + " Rupees : " + f.format(dollar) + " Dollars");
+ 
+			pound = amount / 88;
+			System.out.println("Your " + amount + " Rupees : " + f.format(pound) + " Pounds");
+ 
+			euro = amount / 80;
+			System.out.println("Your " + amount + " Rupees : " + f.format(euro) + " Euros");
+ 
+			yen = amount / 0.63;
+			System.out.println("Your " + amount + " Rupees : " + f.format(yen) + " Yen");
+ 
+		} else if (code == 2) {
+			// For Dollar Conversion
+ 
+			rupee = amount * 70;
+			System.out.println("Your " + amount + " Dollars : " + f.format(rupee) + " Rupees");
+ 
+			pound = amount * 0.78;
+			System.out.println("Your " + amount + " Dollars : " + f.format(pound) + " Pounds");
+ 
+			euro = amount * 0.87;
+			System.out.println("Your " + amount + " Dollars : " + f.format(euro) + " Euros");
+ 
+			yen = amount * 111.087;
+			System.out.println("Your " + amount + " Dollars : " + f.format(yen) + " Yen");
+
+		} else if (code == 3) {
+			// For Pound Conversion
+ 
+			rupee = amount * 88;
+			System.out.println("Your " + amount + " Pounds : " + f.format(rupee) + " Rupees");
+ 
+			dollar = amount * 1.26;
+			System.out.println("Your " + amount + " Pounds : " + f.format(dollar) + " Dollars");
+ 
+			euro = amount * 1.10;
+			System.out.println("Your " + amount + " Pounds : " + f.format(euro) + " Euros");
+ 
+			yen = amount * 140.93;
+			System.out.println("Your " + amount + " Pounds : " + f.format(yen) + " Yen");
+ 
+		} else if (code == 4) {
+			// For Euro Conversion
+ 
+			rupee = amount * 80;
+			System.out.println("Your " + amount + " Euros : " + f.format(rupee) + " Rupees");
+ 
+			dollar = amount * 1.14;
+			System.out.println("Your " + amount + " Euros : "  + f.format(dollar) + " Dollars");
+ 
+			pound = amount * 0.90;
+			System.out.println("Your " + amount + " Euros : " + f.format(pound) + " Pounds");
+ 
+			yen = amount * 127.32;
+			System.out.println("Your " + amount + " Euros : " + f.format(yen) + " Yen");
+ 
+		} else if (code == 5) {
+ 
+			// For Yen Conversion
+ 
+			rupee = amount * 0.63;
+			System.out.println("Your " + amount + " Yen : " + f.format(rupee) + " Rupees");
+ 
+			dollar = amount * 0.008;
+			System.out.println("Your " + amount + " Yen : " + f.format(dollar) + " Dollars");
+ 
+			pound = amount * 0.007;
+			System.out.println("Your " + amount + " Yen : " + f.format(pound) + " Pounds");
+ 
+			euro = amount * 0.007;
+			System.out.println("Your " + amount + " Yen : " + f.format(euro) + " Euros");
+		} 
+		else {
+			System.out.println("Invalid Input");
+		}
+	}
 
   public static void add_command() {
 
