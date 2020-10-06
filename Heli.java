5d2ce95a56078e18f539b78139df7222e40b6f27
Nathaniel Cherian <ngcherian2014@gmail.com>
2020-10-06 08:56:26 -0700

    heli

--- Heli.java
+++ Heli.java
@@ -30,20 +30,39 @@ public class Heli extends BaseAnim {
             clear();
             for(int n = 0; n < 27; n++) {
 
-                System.out.println("___________________________    _________________________");
-                System.out.println("`--------------------------`()'--------------------------'");
+                String s = printrotor(n, "_");
+                String b = new String(new char[27-n]).replace("\0", " ");
+                System.out.println(b+s+"    "+s+b);
+                s = printrotor(n, "-");
+                System.out.println(b+s+"`()'"+s+b);
+                heli();
+
+                sleep(50);
+
                 System.out.println("\u001B[2J");
                 String cursor_move = BUILDER_ESCAPE + "0;0H";
                 System.out.print(cursor_move+"\033[0m");
             }
-            for(int p = 27; p > 0; p--) {
-                
+
+            for(int n = 27; n > 0; n--) {
+                String s = printrotor(n, "_");
+                String b = new String(new char[27-n]).replace("\0", " ");
+                System.out.println(b+s+"    "+s+b);
+                s = printrotor(n, "-");
+                System.out.println(b+s+"`()'"+s+b);
+                heli();
+
+                sleep(40);
+
+                System.out.println("\u001B[2J");
+                String cursor_move = BUILDER_ESCAPE + "0;0H";
+                System.out.print(cursor_move+"\033[0m");
             }
         }  
     }
     public String printrotor(int x, String s) {
-
         String rotorlength = new String(new char[x]).replace("\0", s);
         return rotorlength;
     }
+
 }
